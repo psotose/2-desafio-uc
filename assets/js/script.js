69de463d481ce18929abc441dde939a0cd1ce8e2
Patricia <patricia.soto@routinguc.com>
2022-05-22 12:57:05 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// swipperjs
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+},
+});
+
+// jquery
+
+$(document).ready(main);
+
+counter = 1;
+
+function main() {
+  $('.navbar__menu-burguer').click(function(){
+
+  });
+};
